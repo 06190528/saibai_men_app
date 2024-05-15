c9ac4357e4b1c8cd07932a321eb095597f4e48d7
北野篤輝 <meisnit@gmail.com>
2024-05-15 15:57:03 +0900

    Androidのビルド

--- android/app/src/main/kotlin/com/example/saibai_men_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/saibai_men_app/MainActivity.kt
@@ -1,39 +1,6 @@
 package com.example.saibai_men_app
+
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-import androidx.core.content.ContextCompat
-import com.google.android.play.core.review.ReviewManagerFactory
 
 class MainActivity: FlutterActivity() {
-    private val CHANNEL = "app.channel.shared/review"
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-            call, result ->
-            if (call.method == "requestReview") {
-                requestReview()
-                result.success(null)
-            } else {
-                result.notImplemented()
-            }
-        }
-    }
-
-    private fun requestReview() {
-        val manager = ReviewManagerFactory.create(this)
-        val request = manager.requestReviewFlow()
-        request.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val reviewInfo = task.result
-                val flow = manager.launchReviewFlow(this, reviewInfo)
-                flow.addOnCompleteListener { _ ->
-                    // The flow has finished. The API does not indicate whether the user
-                    // reviewed or not, or even whether the review dialog was shown.
-                }
-            }
-        }
-    }
 }
